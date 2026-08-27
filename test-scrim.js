b77186bfbc5eae1a79e83d684004aef2b27033c4
praveen70140 <me@itspraveen.in>
2026-08-27 21:27:53 +0530

    chore: remove junk test/patch files from tracking, update .gitignore

--- test-scrim.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const zlib = require('zlib');
-const buf = fs.readFileSync(process.argv[2]);
-const unzipped = zlib.gunzipSync(buf);
-console.log(JSON.stringify(JSON.parse(unzipped.toString()), null, 2));
